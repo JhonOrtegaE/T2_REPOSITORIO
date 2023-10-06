c2a54148fb0510ab86137dcfc6935cfc07cde6f6
JhonOrtegaE <142622512+JhonOrtegaE@users.noreply.github.com>
2023-10-06 13:51:54 -0500

    Add files via upload

---
+++ src/GestorMarioPerez1/T2_JhonOrtegaEchenique.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package GestorMarioPerez1;
+import TurnosPantallas.MarioClases;
+
+/**
+ *
+ * @author USER
+ */
+public class T2_JhonOrtegaEchenique {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+          //Inicio de la ventana de gestor de citas
+        MarioClases frameprincipal = new MarioClases();
+        frameprincipal.setLocationRelativeTo(null);
+        frameprincipal.setVisible(true);
+    }
+    
+}
